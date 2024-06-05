50de0b3a8b889a85007077ccd5fa659a44494fc9
Emir <buldukemir1@gmail.com>
2024-06-05 21:56:29 +0300

    Some kind of Loading screen

---
+++ GameUI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by buldu on 5.06.2024.
+//
+
+#include "GameUI.h"
---
+++ GameUI.h
@@ -0,0 +1,14 @@
+//
+// Created by buldu on 5.06.2024.
+//
+
+#ifndef KONEKO_GAMEUI_H
+#define KONEKO_GAMEUI_H
+
+
+class GameUI {
+
+};
+
+
+#endif //KONEKO_GAMEUI_H
